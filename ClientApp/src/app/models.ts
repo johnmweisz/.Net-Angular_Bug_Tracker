96c2d5838e05c56fe0a4b218a1de421c08e9ebf4
John Weisz <johnmweisz@gmail.com>
2019-10-18 14:50:33 -0700

    made all model interface properties nullable to allow flexability

--- ClientApp/src/app/models.ts
+++ ClientApp/src/app/models.ts
@@ -1,46 +1,46 @@
 export interface User {
-  UserId: number;
-  FirstName: string;
-  LastName: string;
-  Email: string;
+  UserId?: number;
+  FirstName?: string;
+  LastName?: string;
+  Email?: string;
   Password?: string;
   Confirm?: string;
-  CreatedAt: Date;
-  UpdatedAt: Date;
+  CreatedAt?: Date;
+  UpdatedAt?: Date;
   Assigned?: Bug[];
   Created?: Bug[];
   Comments?: Comment[];
 }
 
 export interface Bug {
-  BugId: number;
-  Subject: string;
-  Description: string;
-  Priority: string;
-  Status: string;
-  DueDate: string;
-  CreatedAt: Date;
-  UpdatedAt: Date;
-  UserId: number;
-  Creator: User;
-  ProjectId: number;
-  Project: Project;
+  BugId?: number;
+  Subject?: string;
+  Description?: string;
+  Priority?: string;
+  Status?: string;
+  DueDate?: string;
+  CreatedAt?: Date;
+  UpdatedAt?: Date;
+  UserId?: number;
+  Creator?: User;
+  ProjectId?: number;
+  Project?: Project;
   Assigned?: User[];
   Comments?: Comment[];
 }
 
 export interface Project {
-  ProjectId: number;
-  Name: string;
+  ProjectId?: number;
+  Name?: string;
 }
 
 export interface Comment {
-  CommentId: number;
-  Status: string;
-  Message: string;
-  BugId: number;
-  UserId: number;
-  CreatedAt: Date;
+  CommentId?: number;
+  Status?: string;
+  Message?: string;
+  BugId?: number;
+  UserId?: number;
+  CreatedAt?: Date;
   Bug?: Bug;
   User?: User;
 }
