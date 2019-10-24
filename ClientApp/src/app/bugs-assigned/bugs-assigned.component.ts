9d02d04222ed522cde2fc5d37b8c08a2f08253a5
John Weisz <johnmweisz@gmail.com>
2019-10-24 15:54:48 -0700

    finished bug-assigned component backend

--- ClientApp/src/app/bugs-assigned/bugs-assigned.component.ts
+++ ClientApp/src/app/bugs-assigned/bugs-assigned.component.ts
@@ -1,8 +1,9 @@
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { BugService } from './../services/bug.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Bug } from '../models';
+import { Router } from '@angular/router';
+import { ProjectService } from '../services/project.service';
+import { Subscription } from 'rxjs';
+import { Project, Bug } from '../models';
 
 @Component({
   selector: 'app-bugs-assigned',
@@ -10,18 +11,29 @@ import { Bug } from '../models';
   styleUrls: ['./bugs-assigned.component.css']
 })
 export class BugsAssignedComponent implements OnInit, OnDestroy {
+  private projectSub: Subscription;
   private bugListSub: Subscription;
+  public project: Project;
   public bugs: Bug[];
 
   constructor(
-    private _bugs: BugService,
+    private _project: ProjectService,
+    private _bug: BugService,
     private _router: Router
   ) { }
 
   ngOnInit() {
     if (JSON.parse(localStorage.getItem('user')) != null) {
-      this._bugs.getAssigned(JSON.parse(localStorage.getItem('user')).UserId);
-      this.bugListSub = this._bugs.bugList.subscribe(b => this.bugs = b);
+      const UserId = JSON.parse(localStorage.getItem('user')).UserId;
+      this.projectSub = this._project.aProject.subscribe(p => {
+        this.project = p;
+        if (this.project != null) {
+          this._bug.getAssigned(UserId, p.ProjectId);
+        } else {
+          this._bug.getAssigned(UserId);
+        }
+      });
+      this.bugListSub = this._bug.bugList.subscribe(b => this.bugs = b);
     } else {
       this._router.navigate(['/']);
     }
@@ -29,6 +41,7 @@ export class BugsAssignedComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.bugListSub.unsubscribe();
+    this.projectSub.unsubscribe();
   }
 
 }
