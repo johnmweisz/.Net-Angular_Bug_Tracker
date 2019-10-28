fc6bfd96d20a9102126e423a189151bc1a698e7a
John Weisz <johnmweisz@gmail.com>
2019-10-28 15:58:59 -0700

    added project-contributor logic

--- ClientApp/src/app/project-contributors/project-contributors.component.ts
+++ ClientApp/src/app/project-contributors/project-contributors.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Project } from '../models';
+import { Project, Contributor } from '../models';
 import { ProjectService } from '../services/project.service';
 
 @Component({
@@ -11,17 +11,35 @@ import { ProjectService } from '../services/project.service';
 export class ProjectContributorsComponent implements OnInit, OnDestroy {
   private projectSub: Subscription;
   public project: Project;
+  public isContributor = false;
+  public isPrivate = true;
 
   constructor(
     private _project: ProjectService
   ) { }
 
   ngOnInit() {
-    this.projectSub = this._project.aProject.subscribe(p => this.project = p);
+    this.projectSub = this._project.aProject.subscribe(p => {
+      this.project = p;
+      if (this.project) {
+        this.checkContributor();
+        if (this.project.Public === 0) {
+          this.isPrivate = false;
+        }
+      }
+    });
   }
 
   ngOnDestroy() {
     this.projectSub.unsubscribe();
+  }
+
+  checkContributor() {
+    for (const c of this.project.Contributors) {
+      if (c.UserId === JSON.parse(localStorage.getItem('user')).UserId) {
+        this.isContributor = true;
+      }
+    }
   }
 
 }
