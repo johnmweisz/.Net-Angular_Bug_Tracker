26755da9b88466e01b246d03b20d3dcf54a13a27
John Weisz <johnmweisz@gmail.com>
2019-11-06 10:27:28 -0800

    simplified goBack algo

--- ClientApp/src/app/services/router.service.ts
+++ ClientApp/src/app/services/router.service.ts
@@ -6,24 +6,26 @@ import { Router } from '@angular/router';
 })
 export class RouterService {
   private routes: string[] =  [];
+  private current: string;
 
   constructor(
     private _router: Router
     ) { }
 
-    setRoute(route: string) {
-      if (this.routes[this.routes.length - 1] !== route) {
-        this.routes.push(route);
-      }
+  setRoute(route: string) {
+    if (this.current) {
+      this.routes.push(this.current);
     }
+    this.current = route;
+  }
 
-    goBack() {
-      if (this.routes.length > 1) {
-        this.routes.pop();
-        this._router.navigate([`${this.routes.pop()}`]);
-      } else {
-        this._router.navigate([`/`]);
-      }
+  goBack() {
+    if (this.routes.length > 1) {
+      this.current = null;
+      this._router.navigate([`${this.routes.pop()}`]);
+    } else {
+      this._router.navigate([`/`]);
     }
+  }
 
 }
